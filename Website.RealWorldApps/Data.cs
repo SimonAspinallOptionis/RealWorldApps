f841c61ee7b4206647b29390afc6c421fa377ca7
SimonAspinallOptionis <si-aspinall@hotmail.co.uk>
2015-01-13 15:36:09 +0000

    Add image to the player and change the image size for the coach

--- Website.RealWorldApps/Data.cs
+++ Website.RealWorldApps/Data.cs
@@ -21,19 +21,19 @@ namespace Website.RealWorldApps
 
         private void AddPlayers()
         {
-            Players.Add(new Player { Name = "Elliot Cochrane", JerseyNumber = 4, Position = "Guard" });
-            Players.Add(new Player { Name = "Harvey Roberts", JerseyNumber = 5, Position = "Guard" });
-            Players.Add(new Player { Name = "Finlay Williams", JerseyNumber = 6, Position = "Guard" });
-            Players.Add(new Player { Name = "Alex Hawkins", JerseyNumber = 7, Position = "Forward" });
-            Players.Add(new Player { Name = "Zak Williams", JerseyNumber = 8, Position = "Guard" });
-            Players.Add(new Player { Name = "Lloyd Hollins", JerseyNumber = 9, Position = "Forward" });
-            Players.Add(new Player { Name = "Adam Chedd", JerseyNumber = 10, Position = "Forward" });
-            Players.Add(new Player { Name = "Rowan Ladd", JerseyNumber = 11, Position = "Forward" });
-            Players.Add(new Player { Name = "Jacob Dobbins", JerseyNumber = 12, Position = "Guard" });
-            Players.Add(new Player { Name = "Sean Mottram", JerseyNumber = 13, Position = "Forward" });
-            Players.Add(new Player { Name = "Josh McGarven", JerseyNumber = 14, Position = "Forward" });
-            Players.Add(new Player { Name = "Callum Settle", JerseyNumber = 15, Position = "Center" });
-            Players.Add(new Player { Name = "Nathan Dobbins", JerseyNumber = 23, Position = "Guard" });
+            Players.Add(new Player { Name = "Elliot Cochrane", JerseyNumber = 4, Position = "Guard", ImgName="CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Harvey Roberts", JerseyNumber = 5, Position = "Guard", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Finlay Williams", JerseyNumber = 6, Position = "Guard", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Alex Hawkins", JerseyNumber = 7, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Zak Williams", JerseyNumber = 8, Position = "Guard", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Lloyd Hollins", JerseyNumber = 9, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Adam Chedd", JerseyNumber = 10, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Rowan Ladd", JerseyNumber = 11, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Jacob Dobbins", JerseyNumber = 12, Position = "Guard", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Sean Mottram", JerseyNumber = 13, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Josh McGarven", JerseyNumber = 14, Position = "Forward", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Callum Settle", JerseyNumber = 15, Position = "Center", ImgName = "CheshireWire.jpg" });
+            Players.Add(new Player { Name = "Nathan Dobbins", JerseyNumber = 23, Position = "Guard", ImgName = "CheshireWire.jpg" });
         }
 
         private void AddCoaches()
